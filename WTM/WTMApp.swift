dab67f4761a7ef81fec154d03658aeef95e87bb9
mattguoo <guomr@bc.edu>
2023-04-24 14:21:10 -0400

    Initial Commit

---
+++ WTM/WTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  WTMApp.swift
+//  WTM
+//
+//  Created by Matt Guo on 4/24/23.
+//
+
+import SwiftUI
+
+@main
+struct WTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
